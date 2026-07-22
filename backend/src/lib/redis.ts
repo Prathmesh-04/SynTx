af735ed162c80b680d20cd7edb471b59d51de503
Prathmesh <prathmesh0170s@gmail.com>
2026-07-23 01:46:35 +0530

    adding redis url

--- backend/src/lib/redis.ts
+++ backend/src/lib/redis.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
 
-export const redis = createClient()
+export const redis = createClient({
+    url: process.env.REDIS_URL
+})
 
 redis.on("error" , err => console.log("Error while connecting to redis"))
 
