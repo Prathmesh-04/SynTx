5a3e57b9fbec0bd33e8d390cc819b589c2acddee
Prathmesh <prathmesh0170s@gmail.com>
2026-07-23 02:44:21 +0530

    CORS Fixing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const store = new RedisStore({
 
 app.use(cors({
     origin: ["http://localhost:5173",
-        "https://syn-tx.vercel.app/"
+        "https://syn-tx.vercel.app"
     ],
     credentials: true
 }))
@@ -198,4 +198,8 @@ app.post("/logout", (req,res)=>{
 })
 
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
