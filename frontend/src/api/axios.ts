be969deead98e8a838546f905c3ecd98cee8f21e
Prathmesh <prathmesh0170s@gmail.com>
2026-08-20 22:49:31 +0530

    Fixing the axios error

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = globalThis.process?.env?.VITE_API_URL ?? "http://localhost:3000";
+const apiUrl = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
 
 export const api = axios.create({
     baseURL: apiUrl,
